624ab74cb1b619e02704c1a1b640cb1c73459e0c
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-13 15:26:06 +0100

    Adds #top_up and #balance on OysterCard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,13 @@
 class OysterCard
-  def hello
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
   end
+
+  def top_up(number)
+    @balance += number
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,16 @@
 require 'oystercard'
 
 describe OysterCard do
-  
+
+  describe "#balance" do
+    it "has a starting balance of 0" do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe "#top_up do" do
+    it "tops up by given amout" do
+      expect(subject.top_up(10)).to eq subject.balance
+    end
+  end
 end
