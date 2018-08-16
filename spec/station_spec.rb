8efb8048153c312f457378c18602d362770ae4dc
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-16 17:42:29 +0100

    Adds tests for Station and attr_reader

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,1 +1,16 @@
 require 'station'
+
+RSpec.describe Station do
+
+  # subject { Station.new(name: "Hoxton", zone: 1) }
+  let(:subject) { described_class.new("Hoxton", 1)}
+
+
+  it 'knows its name' do
+    expect(subject.name).to eq("Hoxton")
+  end
+
+  it 'knows its zone' do
+    expect(subject.zone).to eq(1)
+  end
+end
